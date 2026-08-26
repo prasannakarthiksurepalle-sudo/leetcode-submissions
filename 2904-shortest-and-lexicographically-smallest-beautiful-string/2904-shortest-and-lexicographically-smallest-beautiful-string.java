5ad64f151d576d9b71ebfabaa7408a9489ee673a
prasannakarthiksurepalle-sudo <prasannakarthiksurepalle@gmail.com>
2026-08-26 12:17:12 +0530

    Time: 1 ms (100%), Space: 44 MB (58.39%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,27 +1,20 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        List<String> beauty=new ArrayList<>();
+        int left=0;
         int n=s.length();
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            if(s.charAt(i)=='1') cnt++;
-            if(cnt==k) beauty.add(s.substring(i,i+1));
-            for(int j=i+1;j<n;j++){
-                if(s.charAt(j)=='1') cnt++;
-                if(cnt==k) beauty.add(s.substring(i,j+1));
-                if(cnt>k) break;
+        String ans="";
+        int ones=0;
+        for(int right=0;right<n;right++){
+            if(s.charAt(right)=='1') ones++;
+            if(ones==k){
+                while(s.charAt(left)=='0') left++;
+                String res=s.substring(left,right+1);
+                if(ans.isEmpty() || res.length()<ans.length() || (res.length()==ans.length() && ans.compareTo(res) > 0 )) ans=res;
+
+                left++;
+                ones--;  
             }
         }
-        int minLen=101;
-        for(String x:beauty){
-            minLen=Math.min(minLen,x.length());
-        }
-        List<String> rem=new ArrayList<>();
-        for(String x:beauty){
-            if(x.length() != minLen) rem.add(x);
-        }
-        for(String x:rem) beauty.remove(x);
-        Collections.sort(beauty);
-        return (beauty.size()>0)?beauty.get(0):"";
+        return ans;
     }
 }
