c9b14c215bb703feae00bee2e0a288217dc455b4
prasannakarthiksurepalle-sudo <prasannakarthiksurepalle@gmail.com>
2026-08-26 11:19:46 +0530

    Time: 35 ms (42.71%), Space: 60.4 MB (7.35%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,6 +4,7 @@ class Solution {
         int n=nums.length;
         Arrays.sort(nums);
         for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1,k=n-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
@@ -16,6 +17,8 @@ class Solution {
                     ls.add(nums[k]);
                     st.add(ls);
                     j++;k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
